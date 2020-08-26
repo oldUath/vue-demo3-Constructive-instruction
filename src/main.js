18a8c9bd9ef8c2ca957e495f419498adb4d2c85d
oldDong <2232890213@qq.com>
2020-08-26 12:13:43 +0800

    '绑定事件原理'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+import Vue from 'vue/dist/vue.js'
 Vue.config.productionTip = false
-
 new Vue({
-  render: h => h(App),
+   directives:{
+   on2:{
+       //info参数获取指令的信息
+           inserted(el,info){
+           console.log(info)
+           console.log(info.arg)
+           el.addEventListener(info.arg,info.value)
+         },
+     //用完把监听事件取消掉
+         unbind(el,info){
+         el.addEventListener(info.arg,info.value)
+       }
+     }
+ },
+template:`
+ +     <button v-on2:click="hi">点我</button>
+  `,
+    methods:{
+  hi(){
+    console.log("hi")
+  }
+}
 }).$mount('#app')
+
